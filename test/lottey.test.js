07fb8d865d5c6ee267372e4b9c141f4c6be1ed4e
CraftyDragon678 <craftydragon678@gmail.com>
2021-01-15 10:46:11 +0900

    lottery test

---
+++ test/lottey.test.js
@@ -0,0 +1,18 @@
+const Lottery = artifacts.require("Lottery");
+
+contract('Lottery', ([deployer, user1, user2]) => {
+  let lottery;
+  beforeEach(async () => {
+    console.log('Before each');
+    lottery = await Lottery.new();
+  });
+
+  it('Basic test', async () => {
+    console.log('Basic test');
+    let owner = await lottery.owner();
+    let value = await lottery.getSomeValue();
+    console.log(`owner: ${owner}`);
+    console.log(`value: ${value}`);
+    assert.equal(value, 5);
+  });
+});
