d2a845b9ea0a3697395a3f9601ed51828e2b4d62
CraftyDragon678 <craftydragon678@gmail.com>
2021-01-15 11:50:05 +0900

    distribute

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -19,6 +19,9 @@ contract Lottery {
   uint256 constant internal BET_AMOUNT = 5 * 10 ** 15;
   uint256 private _pot;
 
+  enum BlockStatus { Checkable, NotRevealed, BlockLimitPassed }
+  enum BettingResult { Fail, Win, Draw }
+
   event BET(uint256 index, address bettor, uint256 amount, byte challenges, uint256 answerBlockNumber);
 
   constructor() public {
@@ -45,6 +48,84 @@ contract Lottery {
     // emit event
     emit BET(_tail - 1, msg.sender, msg.value, challenges, block.number + BET_BLOCK_INTERVAL);
     return true;
+  }
+
+  function distribute() public {
+    uint256 cur;
+    BetInfo memory b;
+    BlockStatus currentBlockStatus;
+    for (cur = _head; cur < _tail; cur++) {
+      b = _bets[cur];
+      currentBlockStatus = getBlockStatus(b.answerBlockNumber);
+
+      if (currentBlockStatus == BlockStatus.Checkable) {
+        // if win, bettor gets pot
+        // if fail, bettor's money goes pot
+        // if draw, refund bettor's money
+      }
+
+      if (currentBlockStatus == BlockStatus.NotRevealed) {
+        break;
+      }
+      
+      if (currentBlockStatus == BlockStatus.BlockLimitPassed) {
+        // refund
+        // emit refund
+      }
+      popBet(cur);
+    }
+  }
+
+  /**
+   * @dev get result of bet
+   * @param challenges bet character
+   * @param answer blockhash
+   * @return result
+   */
+  function isMatch(byte challenges, bytes32 answer) public pure returns (BettingResult) {
+    // challenges 0xab
+    // answer 0xab.....ff  32bytes
+
+    byte c1 = challenges;
+    byte c2 = challenges;
+    
+    byte a1 = answer[0];
+    byte a2 = answer[0];
+
+    // get first number
+    c1 = c1 >> 4;
+    c1 = c1 << 4;
+    a1 = a1 >> 4;
+    a1 = a1 << 4;
+
+    // get second number
+    c2 = c2 << 4;
+    c2 = c2 >> 4;
+    a2 = a2 << 4;
+    a2 = a2 >> 4;
+
+    if (a1 == c1 && a2 == c2) {
+      return BettingResult.Win;
+    }
+    
+    if (a1 == c1 || a2 == c2) {
+      return BettingResult.Draw;
+    }
+
+    return BettingResult.Fail;
+  }
+
+  function getBlockStatus(uint256 answerBlockNumber) internal view returns (BlockStatus) {
+    if (block.number > answerBlockNumber && block.number < BLOCK_LIMIT + answerBlockNumber) {
+      return BlockStatus.Checkable;
+    }
+    if (block.number <= answerBlockNumber) {
+      return BlockStatus.NotRevealed;
+    }
+    if (block.number >= answerBlockNumber + BLOCK_LIMIT) {
+      return BlockStatus.BlockLimitPassed;
+    }
+    return BlockStatus.BlockLimitPassed;
   }
 
   function getBetInfo(uint256 index) public view returns (
