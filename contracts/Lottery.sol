dbdb6ba836fa7ae1a17e2192e5cb21cd74f6a645
CraftyDragon678 <craftydragon678@gmail.com>
2021-01-15 10:48:14 +0900

    bet info

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,7 +2,21 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Lottery {
+  struct BetInfo {
+    uint256 answerBlockNumber;
+    address payable bettor;
+    byte challenges;
+  }
+
+  uint256 private _tail;
+  uint256 private _head;
+  mapping (uint256 => BetInfo) private _bets;
+
   address public owner;
+
+  uint256 constant internal BLOCK_LIMIT = 256;
+  uint256 constant internal BET_BLOCK_INTERVAL = 3;
+  uint256 constant internal BET_AMOUNT = 5 * 10 ** 15;
   uint256 private _pot;
   constructor() public {
     owner = msg.sender;
@@ -14,5 +28,33 @@ contract Lottery {
 
   function getPot() public view returns (uint256 value) {
     return _pot;
+  }
+
+  function getBetInfo(uint256 index) public view returns (
+    uint256 answerBlockNumber,
+    address bettor,
+    byte challenges
+  ) {
+    BetInfo memory b = _bets[index];
+    answerBlockNumber = b.answerBlockNumber;
+    bettor = b.bettor;
+    challenges = b.challenges;
+  }
+
+  function pushBet(byte challenges) public returns (bool) {
+    BetInfo memory b;
+    b.bettor = msg.sender;
+    b.answerBlockNumber = block.number + BET_BLOCK_INTERVAL;
+    b.challenges = challenges;
+
+    _bets[_tail] = b;
+    _tail++;
+
+    return true;
+  }
+
+  function popBet(uint256 index) public returns (bool) {
+    delete _bets[index];
+    return true;
   }
 }
